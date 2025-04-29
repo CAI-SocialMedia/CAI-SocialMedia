03685145440612a7f413222655cd67047c144aab
gokhangnccn <gokhangnccn@gmail.com>
2025-04-29 16:44:57 +0300

    tkonsolda token gözükmesi için log eklendi

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,5 +7,7 @@ export async function authenticate(email, password, isRegister = false) {
         : await signInWithEmailAndPassword(auth, email, password);
 
     const token = await result.user.getIdToken();
+    // Geçici olarak ekledik. SONRA SİL
+    console.log("Token:", token);
     return { token, user: result.user };
 }
