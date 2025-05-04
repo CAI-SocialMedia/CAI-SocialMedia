8ff669d9c57e9b793afe47c2df9946810ad8800b
gokhangnccn <gokhangnccn@gmail.com>
2025-05-04 16:27:26 +0300

    Firebase hosting ve Firestore CD entegrasyonu tamamlandı

--- frontend/src/auth/firebase.js
+++ frontend/src/auth/firebase.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAHMbljcCsZPKNQBFhVTEeIn_Vvci8Eyd4",
     authDomain: "cai-socialmedia.firebaseapp.com",
     projectId: "cai-socialmedia",
+    databaseURL: "https://cai-socialmedia-default-rtdb.europe-west1.firebasedatabase.app",
     storageBucket: "cai-socialmedia.firebasestorage.app",
     messagingSenderId: "237279331001",
     appId: "1:237279331001:web:34421de9402083f8ce1168",
