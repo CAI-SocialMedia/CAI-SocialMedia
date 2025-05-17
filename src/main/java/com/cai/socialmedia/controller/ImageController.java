de171fb38120a70edb480d3ed9c554b9ea50fe8a
FatmaAleyna <ftmalyndgn23@gmail.com>
2025-05-17 16:26:43 +0300

    request, GenerateImageRequestDTO'dan alınacak şekilde güncellendi.

--- src/main/java/com/cai/socialmedia/controller/ImageController.java
+++ src/main/java/com/cai/socialmedia/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.cai.socialmedia.controller;
 
+import com.cai.socialmedia.dto.GenerateImageRequestDTO;
 import com.cai.socialmedia.model.PostDocument;
 import com.cai.socialmedia.service.ReplicateService;
 import com.cai.socialmedia.util.ApiResponse;
@@ -12,14 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/images")
 @RequiredArgsConstructor
 public class ImageController {
+
     private final ReplicateService replicateService;
 
     @PostMapping("/generate")
-    public ResponseEntity<ApiResponse<PostDocument>> generateImage(@RequestBody GenerateImageRequest requestBody) {
+    public ResponseEntity<ApiResponse<PostDocument>> generateImage(@RequestBody GenerateImageRequestDTO requestBody) {
         String userUid = SecurityUtil.getAuthenticatedUidOrThrow();
-        PostDocument post = replicateService.generateImage(requestBody.prompt(), userUid);
+        PostDocument post = replicateService.generateImage(requestBody, userUid);
         return ResponseEntity.ok(ApiResponse.success(post));
     }
 }
-
-record GenerateImageRequest(String prompt) {}
