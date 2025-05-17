e989d38836b16644efbda6ca1ae4b4d696c78d54
FatmaAleyna <ftmalyndgn23@gmail.com>
2025-05-17 16:13:10 +0300

    CaptionUpdateRequestDTO oluşturuldu.

---
+++ src/main/java/com/cai/socialmedia/dto/CaptionUpdateRequestDTO.java
@@ -0,0 +1,12 @@
+package com.cai.socialmedia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaptionUpdateRequestDTO {
+    private String caption;
+}
